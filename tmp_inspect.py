3a88f64e84d2625cf526ea67eabd32f15a26dedb
Deejulu <davidnetflix0011@gmail.com>
2026-05-08 18:33:20 +0100

    chore: Remove redundant debug, temp and duplicate files

--- tmp_inspect.py
+++
@@ -1,4 +0,0 @@
-with open('orders/views.py','rb') as f:
-    lines = f.readlines()
-for i in range(156,168):
-    print(i+1, lines[i])
--- tmp_print.py
+++
@@ -1,5 +0,0 @@
-f=open('orders/views.py','rb')
-lines=f.readlines()
-for i in range(90,176):
-    print(i+1, repr(lines[i]))
-f.close()
